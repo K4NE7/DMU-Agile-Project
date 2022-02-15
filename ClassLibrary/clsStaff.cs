eb6a44254f6d30b2ae8b8ad05e697f76c4057aa0
Oscar Soanes <oscarsoanes@gmail.com>
2022-02-15 18:10:20 +0000

    Created tstStaff

---
+++ ClassLibrary/clsStaff.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsStaff
+    {
+        public bool administrator { get; set; }
+        public DateTime staffDOB { get; set; }
+        public string staffFullName { get; set; }
+        public string staffEmail { get; set; }
+        public string staffPassword { get; set; }
+        public double staffSalary { get; set; }
+        public int staffId { get; set; }
+    }
+}
---
+++ Testing1/UnitTest1.cs
@@ -0,0 +1,112 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            // Create instance of Staff
+            clsStaff staffMember = new clsStaff();
+
+            Assert.IsNotNull(staffMember);
+        }
+
+        [TestMethod]
+        public void AdministratorStaffOK()
+        {
+            // Create instance of Staff
+            clsStaff staffMember = new clsStaff();
+
+            Boolean TestData = true;
+            // Assign TestData to property
+            staffMember.administrator = TestData;
+
+            Assert.AreEqual(staffMember.administrator, TestData);
+        }
+
+        [TestMethod]
+        public void DOBStaffOK()
+        {
+            // Create instance of Staff
+            clsStaff staffMember = new clsStaff();
+
+            DateTime TestData = DateTime.Now.AddDays(-5840);
+            // Assign TestData to property
+            staffMember.staffDOB = TestData;
+
+            Assert.AreEqual(staffMember.staffDOB, TestData);
+
+        }
+
+        [TestMethod]
+        public void FullNameStaffOK()
+        {
+            // Create instance of Staff
+            clsStaff staffMember = new clsStaff();
+
+            string TestData = "John Smith";
+            // Assign TestData to property
+            staffMember.staffFullName = TestData;
+
+            Assert.AreEqual(staffMember.staffFullName, TestData);
+        }
+
+        [TestMethod]
+        public void EmailStaffOK()
+        {
+            // Create instance of Staff
+            clsStaff staffMember = new clsStaff();
+
+            string TestData = "johnsmith@kores.co.uk";
+            // Assign TestData to property
+            staffMember.staffEmail = TestData;
+
+            Assert.AreEqual(staffMember.staffEmail, TestData);
+        }
+
+        [TestMethod]
+        public void PasswordStaffOK()
+        {
+            // Create instance of Staff
+            clsStaff staffMember = new clsStaff();
+
+            string TestData = "password123";
+            // Assign TestData to property
+            staffMember.staffPassword = TestData;
+
+            Assert.AreEqual(staffMember.staffPassword, TestData);
+        }
+
+        [TestMethod]
+        public void SalaryStaffOK()
+        {
+            // Create instance of Staff
+            clsStaff staffMember = new clsStaff();
+
+            double TestData = 1999.99;
+            // Assign TestData to property
+            staffMember.staffSalary = TestData;
+
+            Assert.AreEqual(staffMember.staffSalary, TestData);
+        }
+
+        [TestMethod]
+        public void IDStaffOK()
+        {
+            // Create instance of Staff
+            clsStaff staffMember = new clsStaff();
+
+            int TestData = 1;
+            // Assign TestData to property
+            staffMember.staffId = TestData;
+
+            Assert.AreEqual(staffMember.staffId, TestData);
+        }
+    }
+
+}
