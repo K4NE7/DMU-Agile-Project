a182f6466cc6dbb91ef1454464493d00a940f061
Kara Preston <talia.preston123@gmail.com>
2022-02-17 23:48:16 +0000

    Created TestingOrders and clsOrders
    Created testing data to generate the properties for the Order Class and then generated clsOrder.

---
+++ ClassLibrary/clsOrders.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsOrders
+    {
+        public int orderID { get; set; }
+        public int customerID { get; set; }
+        public DateTime orderDate { get; set; }
+        public bool keySent { get; set; }
+        public string extraDetails { get; set; }
+        public int staffID { get; set; }
+    }
+}
---
+++ Testing2/UnitTest1.cs
@@ -0,0 +1,91 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestingOrders
+{
+    [TestClass]
+    public class tstOrders
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsOrders anOrder = new clsOrders();
+
+            Assert.IsNotNull(anOrder);
+        }
+
+        [TestMethod]
+        public void IdOrderOk()
+        {
+            clsOrders anOrder = new clsOrders();
+
+            int testData = 1;
+
+            anOrder.orderID = testData;
+
+            Assert.AreEqual(anOrder.orderID, testData);
+        }
+
+        [TestMethod]
+        public void customerIdOrderOk()
+        {
+            clsOrders anOrder = new clsOrders();
+
+            int testData = 5;
+
+            anOrder.customerID = testData;
+
+            Assert.AreEqual(anOrder.customerID, testData);
+        }
+
+        [TestMethod]
+        public void orderDateOrderOk()
+        {
+            clsOrders anOrder = new clsOrders();
+
+            DateTime testData = DateTime.Now.Date;
+
+            anOrder.orderDate = testData;
+
+            Assert.AreEqual(anOrder.orderDate, testData);
+        }
+
+        [TestMethod]
+        public void keySentOrderOk()
+        {
+            clsOrders anOrder = new clsOrders();
+
+            Boolean testData = true;
+
+            anOrder.keySent = testData;
+
+            Assert.AreEqual(anOrder.keySent, testData);
+        }
+
+        [TestMethod]
+        public void extraDetailsOrderOk()
+        {
+            clsOrders anOrder = new clsOrders();
+
+            String testData = "Steam";
+
+            anOrder.extraDetails = testData;
+
+            Assert.AreEqual(anOrder.extraDetails, testData);
+        }
+
+        [TestMethod]
+        public void staffIdOrderOk()
+        {
+            clsOrders anOrder = new clsOrders();
+
+            int testData = 1;
+
+            anOrder.staffID = testData;
+
+            Assert.AreEqual(anOrder.staffID, testData);
+        }
+
+    }
+}
