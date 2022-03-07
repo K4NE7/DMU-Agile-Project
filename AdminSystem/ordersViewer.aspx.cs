f00c0df8a61dc660f0e1fd0ef6910369c1e9d205
Kara Preston <talia.preston123@gmail.com>
2022-03-07 10:08:21 +0000

    Finished Orders data entry
    Developed a more comprehensive Orders Viewer. Also re-formatted Orders Data Entry and made all fields work for their respective data types and add to the session.

--- AdminSystem/ordersViewer.aspx.cs
+++ AdminSystem/ordersViewer.aspx.cs
@@ -12,6 +12,21 @@ public partial class _1Viewer : System.Web.UI.Page
     {
         clsOrders anOrder = new clsOrders();
         anOrder = (clsOrders)Session["anOrder"];
-        Response.Write(anOrder.orderID);
+      
+        /* Response.Write(anOrder.orderID);
+        Response.Write("\n" + anOrder.customerID);
+        Response.Write("\n" + anOrder.orderDate);
+        Response.Write("\n" + anOrder.keySent);
+        Response.Write("\n" + anOrder.productID);
+        Response.Write("\n" + anOrder.quantity);
+        Response.Write("\n" + anOrder.extraDetails);*/
+
+        lblOrderID.Text += anOrder.orderID;
+        lblCustomerID.Text += anOrder.customerID;
+        lblOrderDate.Text += anOrder.orderDate.Date;
+        lblKeySent.Text += anOrder.keySent;
+        lblProductID.Text += anOrder.productID;
+        lblQuantity.Text += anOrder.quantity;
+        lblExtraDetails.Text += anOrder.extraDetails;
     }
 }
